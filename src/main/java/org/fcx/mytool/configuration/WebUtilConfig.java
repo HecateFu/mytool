93f41108be678666246678ae60981e4edf883db2
HecateFu <hecatefu@outlook.com>
2020-09-23 22:32:08 +0800

    访问githubusercontent增加代理支持

--- src/main/java/org/fcx/mytool/configuration/WebUtilConfig.java
+++ src/main/java/org/fcx/mytool/configuration/WebUtilConfig.java
@@ -2,17 +2,28 @@ package org.fcx.mytool.configuration;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.protocol.HttpContext;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebUtilConfig {
+
     @Profile("pro")
     @Bean
     public Connector httpConnector(){
@@ -42,8 +53,10 @@ public class WebUtilConfig {
         tomcat.addAdditionalTomcatConnectors(connector);
         return tomcat;
     }
+
     @Bean
     public RestTemplate createRestTemplate (RestTemplateBuilder rtb) {
         return rtb.build();
     }
+
 }
